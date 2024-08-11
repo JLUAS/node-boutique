6b06c1b8a23bf75a796323229c198cda3e971282
CHOUGIC LOGIC <jlurquieta1@gmail.com>
2024-08-11 17:00:18 -0600

    Metodos para orden

--- index.js
+++ index.js
@@ -1368,6 +1368,16 @@ app.put('/user/update/orden/:mesa', async (req, res) => {
 
     res.status(200).send('Producto actualizado');
   });
+  const queryMesa = `UPDATE orden_'${mesa}' SET cantidad = ? WHERE mesa = ? and producto = ?`;
+
+  pool.query(queryMesa, [cantidad ,mesa, producto], (err, result) => {
+    if (err) {
+      console.error('Error updating product:', err);
+      return res.status(500).send('Error updating product');
+    }
+
+    res.status(200).send('Producto actualizado');
+  });
 });
 
 app.listen(port, () => {
