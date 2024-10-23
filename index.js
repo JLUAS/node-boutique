88337c33cacb47ea0ddd155cd6d2384950006cfb
JLUAS <jlurquieta1@gmail.com>
2024-10-22 22:43:19 -0600

    cambio en signin user

--- index.js
+++ index.js
@@ -687,7 +687,7 @@ app.post('/admin', (req, res) => {
     connection.query('SELECT * FROM usuarios WHERE nombre = ? or email = ?', [username, username], async (err, results) => {
       connection.release();
       if (err) return res.status(500).send(err);
-      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+      if (!results.length) {
         return res.status(401).send('Nombre de usuario o contraseña incorrecta');
       }
       console.log(results[0].rol);
