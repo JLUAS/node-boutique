b36edeb2be5fde3602315981f8602b2298bb3c8d
JLUAS <jlurquieta1@gmail.com>
2024-10-22 22:45:25 -0600

    cambio en signin user

--- index.js
+++ index.js
@@ -687,11 +687,11 @@ app.post('/admin', (req, res) => {
     connection.query('SELECT * FROM usuarios WHERE nombre = ? or email = ?', [username, username], async (err, results) => {
       connection.release();
       if (err) return res.status(500).send(err);
-      if (!results.length) {
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
         return res.status(401).send('Nombre de usuario o contraseña incorrecta');
       }
       console.log(results[0].rol);
-      if (results[0].rol !== 'admin') {
+      if (results[0].rol !== 'super') {
         return res.status(403).send('Acceso denegado');
       }
       const token = jwt.sign({ id: results[0].id, rol: results[0].rol }, 'secretkey', { expiresIn: '8h' });
